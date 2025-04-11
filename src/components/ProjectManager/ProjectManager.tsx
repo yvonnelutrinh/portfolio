d3c600df764f83824f390bf1ac7538493ed57b9a
yvonnelutrinh <yvonnelutrinh@gmail.com>
2025-04-10 20:25:40 -0400

    add project descriptions, placeholders for text

--- src/components/ProjectManager/ProjectManager.tsx
+++ src/components/ProjectManager/ProjectManager.tsx
@@ -12,49 +12,64 @@ const projectsData = {
     "wander": {
         title: "WANDER",
         description:
-            "A typographic exploration of brutalist design principles, creating a visual language that's both raw and refined.",
-        year: "2023",
-        role: "Design, Typography, Motion",
-        client: "Self-initiated",
+            "Wander is a wellness app designed to combat digital fatigue and unlock imagination through sound and a series of exercises.",
+        year: "2025",
+        role: "UX/UI Design, Audio, Animation, Development",
+        client: "BrainStation | Capstone",
+        details: {subheader: "process", process: "talk about the wander process", impact: "talk about the impact of wander"},
+        url: "https://yvonnelutrinh.github.io/wander/",
+        tech: ["React", "Javascript", "SCSS", "Node", "Express", "SQL", "Gemini API", "Motion", "Tone", "Howler", "Chroma", "Color", "Random-words", "Mobx", "Figma"]
     },
     "creative-world": {
         title: "CREATIVE WORLD",
-        description: "An immersive digital experience that brings cinematic depth and movement to interactive design.",
-        year: "2022",
-        role: "Art Direction, Motion Design",
-        client: "Film Festival",
+        description: "A responsive e-commerce website for Corex Creative's brand expansion project, revealing a less commonly seen side of the brand. Managed entire project lifecycle from user research to Figma prototyping and final implementation in Webflow.",
+        year: "2024",
+        role: "UX/UI Design, No-Code Development",
+        client: "Corex Creative",
+        tech: ["Figma", "Webflow", "HTML", "CSS"],
+        details: {subheader: "process", process: "talk about the process", impact: "talk about the impact"}
     },
     "biz-bot": {
         title: "BIZBOT",
         description:
-            "Experimental typography that pushes the boundaries of legibility while maintaining a strong visual hierarchy.",
-        year: "2023",
-        role: "Typography, Design",
-        client: "Publishing House",
+            "BizBot.AI provides AI adoption recommendations tailored to small business needs, leveraging Gemini to help businesses automate tasks, optimize operations, and make smarter decisions. Built during a 24-hour hackathon by Yvonne Lu Trinh, Ademidé Akinsefunmi, Alexandria Nancoo-Balkaran, Brigid Corey, Toshi Biswas, Quynh Do, and Vivian Cao",
+        year: "2025",
+        role: "Development, Design",
+        client: "Microsoft | BrainStation",
+        url: "https://yvonnelutrinh.github.io/microsoft-hackathon-bizbot/",
+        tech: ["React", "Javascript", "Gemini API", "Figma"],
+        details: {subheader: "process", process: "talk about the process", impact: "talk about the impact"}
     },
     "poke-valentine": {
         title: "POKÉMON VALENTINE",
         description:
-            "A study in using emptiness as a design element, creating tension and focus through strategic use of white space.",
-        year: "2022",
-        role: "Design, Art Direction",
-        client: "Art Gallery",
+            "A simple web app that generates custom downloadable Pokémon Valentine's cards. Built during a 24-hour hackathon by Jiin Park, Zuya Abro, and Yvonne Lu Trinh",
+        year: "2025",
+        role: "Development, Responsive Styling",
+        client: "BrainStation",
+        tech: ["Javascript", "HTML", "SCSS", "PokéAPI", "html2canvas"],
+        url: "https://yvonnelutrinh.github.io/pokemon-valentine/",
+        details: {subheader: "process", process: "talk about the process", impact: "talk about the impact"}
     },
     "development": {
         title: "RESPONSIVE DEVELOPMENT",
         description:
-            "Exploring the intersection of static design and fluid motion, creating a dynamic visual system that responds to user interaction.",
-        year: "2023",
-        role: "Motion Design, Interaction",
-        client: "Tech Startup",
+            "Responsive websites developed from provided designer style guides and mockups.",
+        year: "2025",
+        role: "Development",
+        client: "BrainStation | Projects",
+        tech: ["React", "Javascript", "SCSS", "Node", "Express", "SQL"],
+        details: {subheader: "process", process: "talk about the process", impact: "talk about the impact"}
     },
     "ux": {
         title: "SPORIFY/UX",
         description:
-            "Exploring the intersection of static design and fluid motion, creating a dynamic visual system that responds to user interaction.",
-        year: "2023",
-        role: "Motion Design, Interaction",
-        client: "Tech Startup",
+            "Sporify is an all-in-one learning platform designed to guide users through the art and science of safe mushroom foraging. Sporify addresses real user needs, transforming user research derived pain points into comprehensive functionality within intuitive user experience.",
+        year: "2024",
+        role: "UI/UX Design, Prototyping",
+        client: "OCAD | Projects",
+        tech: ["Figma", "Adobe", "Canva", "Photoshop"],
+        details: {subheader: "process", process: "talk about the process", impact: "talk about the impact"}
     },
 }
 
@@ -108,17 +123,25 @@ export default function ProjectPage() {
 
                                 <div className="grid grid-cols-2 gap-8 text-sm font-mono">
                                     <div>
-                                        <p className="text-gray-500 mb-1">YEAR</p>
+                                        <h2 className="text-gray-500 mb-1">YEAR</h2>
                                         <p>{project.year}</p>
                                     </div>
                                     <div>
-                                        <p className="text-gray-500 mb-1">ROLE</p>
+                                        <h2 className="text-gray-500 mb-1">ROLE</h2>
                                         <p>{project.role}</p>
                                     </div>
                                     <div>
-                                        <p className="text-gray-500 mb-1">CLIENT</p>
+                                        <h2 className="text-gray-500 mb-1">CLIENT</h2>
                                         <p>{project.client}</p>
                                     </div>
+
+                                    {project.tech && (<div>
+                                        <h2 className="text-gray-500 mb-1">TECH STACK</h2>
+                                        <div className="grid grid-cols-2 gap-x-4">
+                                            {project.tech.map((item: string) => (<p className=" text-nowrap">{item}</p>))
+                                            }
+                                        </div>
+                                    </div>)}
                                 </div>
                             </div>
 
@@ -141,14 +164,12 @@ export default function ProjectPage() {
                             </div>
 
                             <div>
-                                <h2 className="font-display text-4xl md:text-6xl mb-8">PROJECT APPROACH</h2>
+                                <h2 className="font-display text-4xl md:text-6xl mb-8">{project.details.subheader}</h2>
                                 <p className="text-xl mb-8">
-                                    The design approach focused on creating tension between brutalist typography and cinematic movement,
-                                    establishing a visual language that's both disruptive and refined.
+                                    {project.details.process}
                                 </p>
                                 <p className="text-xl">
-                                    Each element was carefully considered to balance raw expression with intentional design decisions,
-                                    creating a system that feels both experimental and purposeful.
+                                    {project.details.impact}
                                 </p>
                             </div>
                         </div>
