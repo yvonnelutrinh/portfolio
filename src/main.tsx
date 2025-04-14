8991012084a83d0dc3c5df89b8e934c62a81449a
yvonnelutrinh <yvonnelutrinh@gmail.com>
2025-04-14 02:20:19 -0400

    add deploy

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <BrowserRouter basename={import.meta.env.BASE_URL}>
     <StrictMode>
       <App />
     </StrictMode>
